c38394671f8b688ed68db6101050a146f7cfcdc1
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2022-12-22 20:18:10 +0600

    Updating main.py file

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ import os
 import pandas as pd
 from header_coord_extractor import get_header_coord
 from excel_reader import excel_reader
+from code_price_scrapper import get_code_price
+from object_creator import get_item_list
 
 
+# extarcting content of excel file
 content_df = excel_reader()
+# extracting "Наименование" and "URL" header coordinates
+name_coord_list, url_coord_list = get_header_coord(content_df)
+# obtaining list of objects containing information about items
+items = get_item_list(content_df, name_coord_list, url_coord_list)
 
-print(get_header_coord(content_df))
 
