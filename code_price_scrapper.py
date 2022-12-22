33eaeeb026e31e6897a3d31f2d09673b1b90b798
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2022-12-22 16:47:21 +0600

    Modifying code_price_scrapper.py to extract code and price in string and integer formats

--- code_price_scrapper.py
+++ code_price_scrapper.py
@@ -10,6 +10,11 @@ from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 
 
+# CSS Selector path for code and price of the items
+CODE_CSS_SELECTOR = "div.q-ml-auto.text-ts1.text-color3"
+PRICE_CSS_SELECTOR = "div.text-bold.text-ts5.text-color1"
+
+
 # method that extracts code and price of the items
 def get_code_price(url):
     """
@@ -34,6 +39,24 @@ def get_code_price(url):
     driver = Chrome(executable_path='driver/chromedriver',options=op)
     driver.get(url)     # accessing to url
     
+    
+    # fully item code from website
+    item_code = driver.find_element(By.CSS_SELECTOR, CODE_CSS_SELECTOR)
+    # full item price expression
+    item_price = driver.find_element(By.CSS_SELECTOR, PRICE_CSS_SELECTOR)
+    
+    
+    # extracting numeric code
+    for elem in item_code.text.split():
+        if elem.isdigit():
+            code = elem
+    
+    # converting the price information into integer 
+    price_list = []
+    for elem in item_price.text.split():
+        if elem.isdigit():
+            price_list.append(elem) 
+    price = int("".join(price_list))    # removing space separator
     
     return code, price
     
