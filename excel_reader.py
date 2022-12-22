0dbe81b9eb612fe00754d0577d51ee5ab9edceaa
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2022-12-22 13:53:28 +0600

    Creation of excel_reader method in excel_reader.py file

--- excel_reader.py
+++ excel_reader.py
@@ -3,5 +3,33 @@
 Created on Thu Dec 22 13:44:39 2022
 
 @author: Yerbol
+
+This module reads excel spreadsheet and returns the content as DataFrame
 """
 
+
+import os
+import pandas as pd
+
+
+# directory of excel datasheet
+FILENAME = "TestTask_1_input_1.xlsx"
+DIRECTORY = "./data"
+
+
+# function to read excel sheet and return Pandas dataframe
+def excel_reader():
+    """
+    This function to read excel sheet and return Pandas dataframe
+
+    Returns
+    -------
+    content_df : TYPE
+        DESCRIPTION.
+
+    """
+    
+    directory = os.path.join(DIRECTORY, FILENAME)
+    
+    
+    return content_df
