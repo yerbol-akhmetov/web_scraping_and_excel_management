479b21dc5ef0656ae70c4cc69cf24fde1940d4e4
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2022-12-22 13:34:02 +0600

    Correction of key_words variable name

--- header_coord_extractor.py
+++ header_coord_extractor.py
@@ -29,6 +29,16 @@ def get_header_coord(content_df):
     name_idx_list = []  
     url_idx_list = []
     
-    
+    # looping through each cell of DataFrame to find headers
+    for j in range(content_df.shape[0]):
+        for i in range(content_df.shape[1]):
+            name_idx_i, name_idx_j = 0, 0
+            url_idx_i, url_idx_j = 0, 0
+            # finding cell with "Наименование" content
+            if str(content_df.iloc[j,i]).lower() == KEY_WORDS[0]:
+                name_idx_list.append((j, i))    # appending coordinates
+            # finding cell with item URLs
+            if str(content_df.iloc[j,i]).lower() == KEY_WORDS[1]:
+                url_idx_list.append((j, i))     # appending header coordinates
     
     return name_idx_list, url_idx_list
