cbaa33ee71a8ef905fd5657dad066608024e8612
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2022-12-22 13:41:30 +0600

    Reading excel file and checking the functionality of get_header_coord method

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ Created on Thu Dec 22 13:22:08 2022
 
 
 import os
-import requests
 import pandas as pd
+from header_coord_extractor import get_header_coord
 
+
+FILENAME = "TestTask_1_input_1.xlsx"
+DIRECTORY = "./data"
+content_df = pd.read_excel(os.path.join(DIRECTORY, FILENAME),header=None)
+
+print(get_header_coord(content_df))
 
