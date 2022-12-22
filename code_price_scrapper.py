eb2a123f9e18757daea84a9ef9fcdfd7710448d7
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2022-12-22 16:23:55 +0600

    Adding get_code_price method to scrap data from URL

--- code_price_scrapper.py
+++ code_price_scrapper.py
@@ -6,8 +6,32 @@ Created on Thu Dec 22 16:10:44 2022
 """
 
 
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 
 
+# method that extracts code and price of the items
+def get_code_price(url):
+    """
+    
 
+    Parameters
+    ----------
+    url : URL
+        URL of the item to be scrapped.
+
+    Returns
+    -------
+    code : STRING
+        Unique code of the item (index).
+    price : INTEGER
+        The price of the item.
+
+    """
+    op = ChromeOptions()
+    op.add_argument('headless')     # option to prevent browser opening
+    
+    
+    
+    return code, price
+    
